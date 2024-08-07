8be162d9b71d5b81b6299c870a065166b6f3dd47
usama09-cpu <uusaamaa0901@gmail.com>
2024-08-08 03:27:36 +0800

    update code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './assets/css/index.css'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
